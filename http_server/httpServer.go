2edefac479ad937e9ed54c48dfe8ea95e28fa40a
wuxiang <wuxiangzhou2010@gmail.com>
2018-03-24 19:06:15 +0800

    add http proxy server

---
+++ http_server/httpServer.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	l, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for {
+		client, err := l.Accept()
+		fmt.Println("new request")
+		if err != nil {
+			log.Panic(err)
+		}
+		go handleClientRequest(client)
+	}
+
+}
+
+func handleClientRequest(client net.Conn) {
+	if client == nil {
+		return
+	}
+
+	defer client.Close()
+
+	var b [1024]byte
+	n, err := client.Read(b[:])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var method, host, address string
+	fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
+	fmt.Printf("method = %s,host = %s\n", method, host)
+	hostPortURL, err := url.Parse(host)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if hostPortURL.Opaque == "443" {
+		address = hostPortURL.Scheme + ":443"
+	} else {
+		if strings.Index(hostPortURL.Host, ":") == -1 {
+			address = hostPortURL.Host + ":80"
+		} else {
+			address = hostPortURL.Host
+		}
+	}
+
+	server, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if method == "CONNECT" {
+		fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n\r\n")
+	} else {
+		server.Write(b[:n])
+	}
+
+	go io.Copy(server, client)
+	io.Copy(client, server)
+}
