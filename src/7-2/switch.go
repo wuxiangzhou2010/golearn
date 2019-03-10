27da28c9a32bf91db417c8ea54262cc80ce4441b
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-10 11:11:59 +0800

    update type switch

--- src/7-2/switch.go
+++ src/7-2/switch.go
@@ -1,19 +1,30 @@
 package main
 
+// 1. change seed for random number generator
+// 2. type assertion
+// 3. rand.Seed(int64),  rand.Intn(int) int
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	ia := []interface{}{byte(6), 'a', uint(10), int32(-4)}
-	switch v := ia[rand.Intn(4):]; interface{}(v).(type) {
+	ia := []interface{}{byte(6), 'a', uint(10), int32(-4)} //interface{} array can hold any type of variable
+	rand.Seed(time.Now().UnixNano())                       // change seed, int64
+	// switch v := ia[rand.Intn(4):]; interface{}(v).(type) { // case A
+	switch v := ia[rand.Intn(4)]; interface{}(v).(type) { // case B C D  default
 	case []interface{}:
 		fmt.Println("case A")
 	case byte:
 		fmt.Println("case B")
+	case string:
+		fmt.Println("case C")
+	case uint:
+		fmt.Println("case D")
+	// case int32:
+	// 	fmt.Println("case E")
 	default:
-		fmt.Println("Unknow!")
+		fmt.Println("Unknown!")
 	}
-
 }
