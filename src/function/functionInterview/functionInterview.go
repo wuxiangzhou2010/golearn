93db561dd69e174bfbb6fc06e691eba1086d02c7
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-16 15:11:50 +0800

    add  functionInterview

---
+++ src/function/functionInterview/functionInterview.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type query func(string) string
+
+func exec(name string, vs ...query) string {
+	ch := make(chan string)
+	fn := func(i int) {
+		ch <- vs[i](name)
+	}
+	for i, _ := range vs {
+		go fn(i)
+	}
+	return <-ch // only one value in the channel is reurned and there is goroutine leak
+}
+
+func main() {
+	ret := exec("111", func(n string) string {
+		return n + "func1"
+	}, func(n string) string {
+		return n + "func2"
+	}, func(n string) string {
+		return n + "func3"
+	}, func(n string) string {
+		return n + "func4"
+	})
+	fmt.Println(ret)
+}
+
+/*
+ reference: https://zhuanlan.zhihu.com/p/35058068 issue10
+*/
