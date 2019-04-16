5383235e1fd979de2c3ddee6292c7cf0c6d68fc6
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-16 14:44:09 +0800

    add debug log stack overflow

--- src/pointer/pointerInterview/pointerInterview.go
+++ src/pointer/pointerInterview/pointerInterview.go
@@ -7,7 +7,7 @@ type People struct {
 }
 
 func (p *People) String() string { // 栈空间溢出
-	return fmt.Sprintf("print :%v ", p) // \pointerInterview.go:10:9: Sprintf format %v with arg p causes recursive String method call
+	return fmt.Sprintf("print :%v ", p) // pointerInterview.go:10:9: Sprintf format %v with arg p causes recursive String method call
 }
 func main() {
 	p := &People{}
@@ -17,4 +17,9 @@ func main() {
 /*
 
 reference: https://zhuanlan.zhihu.com/p/35058068 题目4
+
+
+
+runtime: goroutine stack exceeds 1000000000-byte limit
+fatal error: stack overflow
 */
