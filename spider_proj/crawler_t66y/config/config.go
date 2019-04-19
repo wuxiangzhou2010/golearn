516ae312d6fad31059545020ad5958be5b1bd304
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-20 00:10:30 +0800

    more start page

--- spider_proj/crawler_t66y/config/config.go
+++ spider_proj/crawler_t66y/config/config.go
@@ -3,8 +3,9 @@ package config
 import "github.com/wuxiangzhou2010/luandun/go/spider_proj/crawler_t66y/model"
 
 var StartPages = []string{
-	"http://t66y.com/thread0806.php?fid=8",  // 新时代
-	"http://t66y.com/thread0806.php?fid=16", //达盖尔
+	//"http://t66y.com/thread0806.php?fid=8",  // 新时代
+	//"http://t66y.com/thread0806.php?fid=16", //达盖尔
+	"http://t66y.com/thread0806.php?fid=21", //下载区
 }
 
 type Config struct {
